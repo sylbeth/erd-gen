a8055b763877533026e6c23984d49864932cab84
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2025-04-02 22:11:25 +0200

    update main with the changes

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,37 @@
-use std::collections::HashSet;
+use std::{
+    collections::{HashMap, HashSet},
+    error::Error,
+};
 
 use erdgen::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let graph = Graph {
         size: Some(Size::Square(10.0)),
         direction: Some(Direction::LeftRight),
         layout: Some(Layout::Dot),
-        bgcolor: Some(Color::None),
+        bg_color: Some(Color::None),
         fonts: Some(Domain::All(vec![
             "Helvetica".into(),
             "Arial".into(),
             "sans-serif".into(),
         ])),
-        fontcolor: Some(Domain::All(Color::Black)),
+        font_color: Some(Domain::All(Color::Black)),
         entities: Entities {
             kind: EntitiesKind::Chen,
             entities: HashSet::new(),
+            color: HashMap::new(),
+            attributes_color: HashMap::new(),
         },
         relationships: Relationships {
             kind: RelationshipsKind::ChenArrow,
             relationships: HashSet::new(),
+            color: HashMap::new(),
         },
     };
     println!("{:#?}", graph);
     let mut dot = String::new();
     graph.to_dot(&mut dot);
     println!("{}", dot);
+    Ok(())
 }
