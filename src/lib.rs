edfe67e22c11cadca5ab89d2342ff604e6235eff
Sílvia Elisabeth Romero García <sylvielizshadow@gmail.com>
2025-04-02 21:01:38 +0200

    update lib.rs and main.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
-pub mod relationships;
-pub mod entity;
-pub mod graph;
-pub mod entities;
-pub mod relationship;
 pub mod attribute;
+pub mod color;
+pub mod entities;
+pub mod entity;
+pub mod entries;
+pub mod graph;
+pub mod relationship;
+pub mod relationships;
 
 pub mod prelude {
     pub use super::attribute::{Attribute, Composition, Kind as AttributeKind};
-    pub use super::entities::{Kind as EntitiesKind, Entities};
     pub use super::color::Color;
+    pub use super::entities::{Entities, Kind as EntitiesKind};
     pub use super::entity::{Entity, Kind as EntityKind};
-    pub use super::graph::{Graph, Size, Direction};
+    pub use super::entries::{AttributeEntry, EntityEntry, RelationshipEntry};
+    pub use super::graph::{Direction, Domain, Graph, Layout, Size};
     pub use super::relationship::{Kind as RelationshipKind, Multiplicity, Relationship};
     pub use super::relationships::{Kind as RelationshipsKind, Relationships};
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
+use std::collections::HashSet;
+
 use erdgen::prelude::*;
 
 fn main() {
     let graph = Graph {
         size: Some(Size::Square(10.0)),
         direction: Some(Direction::LeftRight),
-        entities: Entities { kind: EntitiesKind::Chen, entities: Vec::new() },
-        relationships: Relationships { kind: RelationshipsKind::ChenArrow, relationships: Vec::new() },
         layout: Some(Layout::Dot),
+        bgcolor: Some(Color::None),
+        fonts: Some(Domain::All(vec![
+            "Helvetica".into(),
+            "Arial".into(),
+            "sans-serif".into(),
+        ])),
+        fontcolor: Some(Domain::All(Color::Black)),
+        entities: Entities {
+            kind: EntitiesKind::Chen,
+            entities: HashSet::new(),
+        },
+        relationships: Relationships {
+            kind: RelationshipsKind::ChenArrow,
+            relationships: HashSet::new(),
+        },
     };
     println!("{:#?}", graph);
     let mut dot = String::new();
